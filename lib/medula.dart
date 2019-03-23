44d826d13d5294df57f0e7a11d88d61944ba5fb5
Rishi Banerjee <rishieric91@gmail.com>
2019-03-24 02:26:35 +0530

    Restructure package format and added exports

---
+++ lib/medula.dart
@@ -0,0 +1,15 @@
+// Exporting Classifier Algorithms
+export 'src/classifier/knn.dart';
+export 'src/classifier/logistic_regression.dart';
+export 'src/classifier/svm.dart';
+export 'src/classifier/tree.dart';
+
+// Exporting Clustering Algorithms
+export 'src/clustering/hierarchical_clustering.dart';
+export 'src/clustering/kmeans.dart';
+
+// Exporting Regressor Algorithms
+export 'src/regressor/linear_regression.dart';
+export 'src/regressor/polynomial_regression.dart';
+export 'src/regressor/svr.dart';
+export 'src/regressor/tree.dart';
--- regressor/tree.dart
+++
@@ -1,3 +0,0 @@
-class DecisionTreeClassifier {}
-
-class RandomForestClassifier {}
